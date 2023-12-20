7e05b7cfd53226beeeb3aa93a6a13bd063d47251
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2023-12-20 17:49:22 +0800

    Revert "Merge branch 'master' of https://github.com/jtlagumbay/adunami"
    This reverts commit f8485bcfdf4c7c1b9280a467549da88214da45e7, reversing
    changes made to db6d77318a9a4b7dc37b0a5f5f0e333998fa98d0.

--- src/main/java/jjprindozo/language/errorHandler.hpp
+++ src/main/java/jjprindozo/language/errorHandler.hpp
@@ -1,4 +1,4 @@
-#pragma once        // prevents multiple inclusions of this header file during compilation
+#pragma once
 #include <iostream>
 using namespace std;
 // Enum of possible error codes
@@ -13,34 +13,27 @@ enum ErrorCode
   ASM_GENERATION // Related to asm generation
 };
 
-// Function to convert ErrorCode enum values to a string for printing
 const char* ErrorCodeToString(ErrorCode t) {
     switch (t) {
-        // Mapping each ErrorCode enum value to its respective string representation
         case SYNTAX: return "SYNTAX";
         case SEMANTIC: return "SEMANTIC";
         case TYPE: return "TYPE";
         case REFERENCE: return "REFERENCE";
         case RUNTIME: return "RUNTIME";
         case INPUT_OUTPUT: return "INPUT_OUTPUT";
-        default: return "ErrorCodeToString ERROR";   // Default case for handling unknown error codes
+        default: return "ErrorCodeToString ERROR";
     }
 }
 
 class Error{
   public:
-    ErrorCode error_code; // type sa error
+    ErrorCode error_code;
     string client_msg; // Maoy makita sa compiler error 
     string trace; // What file and function gave the error
     string possible_cause;
-
-    // constructor for error class
+  
     Error(ErrorCode, string,  string, string);
-
-    // function to print debug info about the error
     void debug();
-
-    // allow easy printing of error objects
     friend ostream &operator<<(ostream &os, const Error &error){
     {
       os << ErrorCodeToString(error.error_code) << " ERROR: " << error.client_msg;
@@ -50,14 +43,12 @@ class Error{
 }
 };
 
-// Definition of the debug method to display detailed error information
 void Error::debug() {
   cout <<endl<< "----------DEBUG MESSAGE----------" << endl;
   cout << ErrorCodeToString(error_code) << " ERROR" << "\nTrace: " << trace << "\nPossible Cause: " << possible_cause <<"\n";
   cout << endl<<"** NOTE: Delete all debug on production **" << endl<<endl;
 }
 
-// Implementation of the constructor for Error class
 Error::Error(ErrorCode code, string m_client_msg, string m_trace, string m_possible_cause) : 
   error_code(code), 
   client_msg(m_client_msg), 
